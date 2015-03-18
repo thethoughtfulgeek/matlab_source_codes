402fba81e995fa298b5ef81ebb6590fc92dc08a1
Jaineel <jaineel@jaineel-VPCEA36FG.(none)>
2015-03-18 18:08:36 -0400

    2nd commit. Added the functionality to produce .txt file data for vertices and edges of polygons.

--- generate_voronoi_own_prg.m
+++ generate_voronoi_own_prg.m
@@ -1,8 +1,8 @@
 clear
 clc
 
-x = rand(1,30);
-y = rand(1,30);
+x = rand(1,50);
+y = rand(1,50);
 pts = [x', y'];
 
 [VX,VY] = voronoi(pts(:,1),pts(:,2));
@@ -31,3 +31,21 @@ Hpl = text(V(2:end,1), V(2:end,2)-0.01, vlabels, ...
       'FontWeight', 'bold', 'HorizontalAlignment',...
       'center', 'BackgroundColor', 'none');
 hold off
+
+
+save('Polygon_Vertices_Coord.txt','V','-ascii')
+
+fileID = fopen('Polygon_Indices.txt','wt+');
+[nrows,ncols] = size(R);
+
+for row = 1:nrows
+    [~,num_indices] = size(R{nrows});
+    %for num_ind = 1 : num_indices
+    formatSpec = '%d ';
+    fprintf(fileID,formatSpec,R{row});
+    %end
+    fprintf(fileID,'\n');
+end
+
+fclose(fileID);
+%type Polygon_Indices.dat
